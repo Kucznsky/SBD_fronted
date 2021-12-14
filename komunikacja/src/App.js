a156be09da1615d812fb47009389619a93f2ca0a
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-12-14 17:57:52 +0100

    _

--- komunikacja/src/App.js
+++ komunikacja/src/App.js
@@ -14,7 +14,7 @@ import {BrowserRouter as  Router, Route, Switch} from 'react-router-dom';
 function App() {
   return (
     <Router>
-      <div className="App"> 
+      <div> 
         <Navbar></Navbar>
         <Switch>
           <Route exact path="/">
