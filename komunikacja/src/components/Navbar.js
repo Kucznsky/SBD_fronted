d5adf55df4a9395f1e2196cec09344b96f42b322
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-12-15 22:13:10 +0100

    icons

--- komunikacja/src/components/Navbar.js
+++ komunikacja/src/components/Navbar.js
@@ -1,6 +1,6 @@
 import {Link} from 'react-router-dom';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome'
-import {faBus,faInfoCircle,faUserFriends,faTicketAlt,faDollarSign} from '@fortawesome/free-solid-svg-icons'
+import {faBus,faInfoCircle,faUserFriends,faTicketAlt,faShoppingCart} from '@fortawesome/free-solid-svg-icons'
 
 const Navbar = () => {
     return(
@@ -8,19 +8,19 @@ const Navbar = () => {
             <nav className='navbar_links'>
                 <ul>
                     <li>
-                        <Link className='link' to='/'>Bus stops</Link>
+                        <Link className='link' to='/'>Bus stops <FontAwesomeIcon icon={faBus}></FontAwesomeIcon></Link>
                     </li>
                     <li>
-                        <Link className='link' to='/bus_info'>Bus info</Link>
+                        <Link className='link' to='/bus_info'>Bus info <FontAwesomeIcon icon={faInfoCircle}></FontAwesomeIcon></Link>
                     </li>
                     <li>
-                        <Link className='link' to='/employee_info'>employee info</Link>
+                        <Link className='link' to='/employee_info'>employee info <FontAwesomeIcon icon={faUserFriends}></FontAwesomeIcon></Link>
                     </li>
                     <li>
-                        <Link className='link' to='/ticket_owners_list'>ticket owners list</Link>
+                        <Link className='link' to='/ticket_owners_list'>ticket owners <FontAwesomeIcon icon={faTicketAlt}></FontAwesomeIcon></Link>
                     </li>
                     <li>
-                        <Link className='link' to='/tickets'>Buy Tickets</Link>
+                        <Link className='link' to='/tickets'>Buy Tickets <FontAwesomeIcon icon={faShoppingCart}></FontAwesomeIcon></Link>
                     </li>
                 </ul>
             </nav>
