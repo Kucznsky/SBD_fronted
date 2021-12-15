e78b1165d143a713fd5bfae48dc5d2dc91deb805
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-12-15 18:44:47 +0100

    bus list and some css

--- komunikacja/src/components/Schedule.js
+++ komunikacja/src/components/Schedule.js
@@ -1,14 +1,51 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
+import PropTypes from 'prop-types';
 import Axios from "axios";
 import {Link} from 'react-router-dom';
 
 const Schedule = () => {
     return (
-        <div className='content' >
-            <p>tu będzie lista numerów autobusów po kliknięciu których wyświetli się rozkład dla danej linii np:</p>
-            <Link to={`/bus_info/$id`}>17</Link>
-        </div>
+        <article className='content' >
+            <section className='list'>
+                {lines.map((lines) => {
+                return(<Line key={lines.id} {...lines}></Line>);
+                })}
+            </section>
+        </article>
     )
 }
 
+const Line = (props) => 
+{
+  const {id} = props;
+  return (
+    <section className='list_element'>
+      <Link className='link3' to={`/bus_info/$id`}>{id}</Link>
+    </section>
+  );
+}
+
+//================================================================
+const lines = [
+
+{
+  id: 1,
+},
+{
+  id: 2,
+},
+{
+  id: 3,
+},
+{
+  id: 4,
+},
+{
+  id: 5,
+},
+{
+  id: 6,
+},
+];
+//================================================================
 export default Schedule
