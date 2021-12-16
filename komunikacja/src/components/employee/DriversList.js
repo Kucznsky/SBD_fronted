dcce9a96cff66b85cfe47a5834b0310e804f030d
Jakub Mońko <jakubm3233@gmail.com>
2021-12-16 22:53:33 +0100

    mamma mia

--- komunikacja/src/components/employee/DriversList.js
+++ komunikacja/src/components/employee/DriversList.js
@@ -1,34 +1,22 @@
-import React from 'react';
+import React,{useEffect,useState} from 'react';
+import Axios from 'axios';
 
 
 
-const data = [
-    {
-        imie: 'andrzejek',
-        nazwisko: 'karaczanski',
-        wynagrodzenie: 1500,
-        idUmowy: 12313,
-        idPracownika: 1,
-    },
-    {
-        imie: 'andr2132k',
-        nazwisko: 'karsnski',
-        wynagrodzenie: 15300,
-        idUmowy: 12313,
-        idPracownika: 1,
-    },
-    {
-        imie: 'andrzejek',
-        nazwisko: 'karaczadasnski',
-        wynagrodzenie: 1500,
-        idUmowy: 12313,
-        idPracownika: 1,
-    },
-]
+
 
 
 
 const DriversList = () => {
+    const [pracownicy,setPracownicy] = useState([])
+
+    useEffect(() => {
+        Axios.get("http://localhost:8080/pracownicy")
+            .then(res => setPracownicy(res.data.pracownicyList));
+    },[]);
+
+
+
     return (
         <fieldset>
             <legend>Lista Kierowców aktualnie zajętych autobusów</legend>
@@ -37,19 +25,19 @@ const DriversList = () => {
                     <div style={{color:'white',width:'20%'}}>Imie</div>
                     <div style={{color:'white',width:'20%'}}>Nazwisko</div>
                     <div style={{color:'white',width:'20%'}}>Wynagrodzenie</div>
-                    <div style={{color:'white',width:'20%'}}>ID umowy</div>
                     <div style={{color:'white',width:'20%'}}>ID Pracownika</div>
                 </div>
                 <hr/>
             {
-                data.map(o => 
+                pracownicy.map(o => o.id_stanowiska=== 5 ? (
                     <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',marginTop:10,width:'100%'}}>
                     <div style={{color:'white',width:'20%'}}>{o.imie}</div>
                     <div style={{color:'white',width:'20%'}}>{o.nazwisko}</div>
                     <div style={{color:'white',width:'20%'}}>{o.wynagrodzenie}</div>
-                    <div style={{color:'white',width:'20%'}}>{o.idUmowy}</div>
-                    <div style={{color:'white',width:'20%'}}>{o.idPracownika}</div>
+                    <div style={{color:'white',width:'20%'}}>{o.id_centrali}</div>
                     </div>
+                    ) : null
+                    
                     )
             }
             
