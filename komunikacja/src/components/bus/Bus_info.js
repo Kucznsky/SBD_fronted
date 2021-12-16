9aa0a8575da6871d885858099a4139ffba3bdfcf
Michal Kuczynski <michal.kuczynski2000@wp.pl>
2021-12-16 23:36:41 +0100

    bus info

--- komunikacja/src/components/bus/Bus_info.js
+++ komunikacja/src/components/bus/Bus_info.js
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React,{useEffect,useState} from 'react';
+import Axios from 'axios';
 
 
 const data = [
@@ -21,6 +21,15 @@ const data = [
 
 
 const Bus_info = () => {
+
+    const [bus,setBus] = useState([])
+
+    useEffect(() => {
+        Axios.get("http://localhost:8080/autobusy")
+            .then(res => setBus(res.data.busList));
+        
+    },[]);
+
     return (
         <fieldset>
             <legend>Lista Wszystkich Autobusów</legend>
@@ -28,13 +37,21 @@ const Bus_info = () => {
                 <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',width:'100%'}}>
                     <div style={{color:'white',width:'15%'}}>id Autobusu</div>
                     <div style={{color:'white',width:'15%'}}>Nr Boczny</div>
+                    <div style={{color:'white',width:'15%'}}>Marka</div>
+                    <div style={{color:'white',width:'15%'}}>Model</div>
+                    <div style={{color:'white',width:'15%'}}>id parkingu</div>
+                    <div style={{color:'white',width:'15%'}}>id centrali</div>
                 </div>
                 <hr/>
             {
-                data.map(o => 
+                bus.map(o => 
                     <div style={{display:'flex',flexDirection:'row',justifyContent:'space-between',marginTop:10,width:'100%'}}>
-                    <div style={{color:'white',width:'15%'}}>{o.idAutobusu}</div>
-                    <div style={{color:'white',width:'15%'}}>{o.nrBoczny}</div>
+                        <div style={{color:'white',width:'15%'}}>{o.id_autobusu}</div>
+                        <div style={{color:'white',width:'15%'}}>{o.numer_boczny}</div>
+                        <div style={{color:'white',width:'15%'}}>{o.nrBoczny}</div>
+                        <div style={{color:'white',width:'15%'}}>{o.nrBoczny}</div>
+                        <div style={{color:'white',width:'15%'}}>{o.id_parkingu}</div>
+                        <div style={{color:'white',width:'15%'}}>{o.id_centrali}</div>
                     </div>
                     )
             }
